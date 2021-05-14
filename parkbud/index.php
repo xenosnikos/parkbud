6827a7f3606f07164a5d68f14f50ec4b7e54c389
alialaghbandrad <ali.alaghbandrad@johnabbottcollege.net>
2021-05-13 23:46:32 -0400

    project setup - index+templates

--- parkbud/index.php
+++ parkbud/index.php
@@ -1,1 +1,17 @@
 <?php
+
+// for development we want to see all the errors, some php.ini versions disable those (e.g. MAMP)
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+session_start();
+
+require_once 'vendor/autoload.php';
+
+require_once 'init.php';
+
+require_once 'account.php';
+
+
+// Run app
+$app->run();
