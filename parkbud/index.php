a86d4da3f02150f6de2a92b938ca9a1f290ac0a6
Philip R <philr89@hotmail.com>
2021-05-14 16:22:37 -0400

    Linked index with master.twig

--- parkbud/index.php
+++ parkbud/index.php
@@ -10,7 +10,7 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
-require_once 'account.php';
+//require_once 'account.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -18,8 +18,8 @@ use Twig\Loader\FilesystemLoader;
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
-echo $twig->render('index.html.twig', []);
+echo $twig->render('index.html.twig', ['title' => 'Parkbud']);
 
 
 // Run app
-$app->run();
+//$app->run();
