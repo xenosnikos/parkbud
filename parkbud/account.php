f5b4f9078e751ef8875fa1cfbc5738c93a908529
alialaghbandrad <ali.alaghbandrad@johnabbottcollege.net>
2021-05-14 22:10:39 -0400

    header

--- parkbud/account.php
+++ parkbud/account.php
@@ -4,35 +4,25 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-// require_once 'account.php';
-
 session_start();
 
 require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
-// require_once 'account.php';
+//require_once 'account.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-// Create and configure Slim app
-$config = ['settings' => [
-    'addContentLengthHeader' => false,
-]];
-$app = new \Slim\App($config);
-
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
-$app->get("/", function ($request, $response, $args) {
-    global $twig;
-    return $response->write($twig->render('index.html.twig', ['title' => 'Parkbud']));
-});
+echo $twig->render('index.html.twig', ['title' => 'Parkbud']);
 
-
-
+echo $twig->render('register.html.twig', ['title' => 'Parkbud']);
 
 // Run app
-$app->run();
+//$app->run();
+
+
