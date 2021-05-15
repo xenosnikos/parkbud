71c9ddb13a0231325cf257f9e2d79db2a4150d7f
alialaghbandrad <ali.alaghbandrad@johnabbottcollege.net>
2021-05-14 23:31:43 -0400

    main + register php

--- parkbud/index.php
+++ parkbud/index.php
@@ -12,27 +12,11 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
+require_once 'main.php';
+
+require_once 'register.php';
+
 // require_once 'account.php';
-
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-
-// Create and configure Slim app
-$config = ['settings' => [
-    'addContentLengthHeader' => false,
-]];
-$app = new \Slim\App($config);
-
-$loader = new FilesystemLoader(__DIR__ . '/templates');
-$twig = new Environment($loader);
-
-$app->get("/", function ($request, $response, $args) {
-    global $twig;
-    return $response->write($twig->render('index.html.twig', ['title' => 'Parkbud']));
-});
-
-
-
 
 // Run app
 $app->run();
---
+++ parkbud/main.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+$app->get("/main", function ($request, $response, $args) {
+    global $twig;
+    return $response->write($twig->render('index.html.twig', ['title' => 'Parkbud']));
+});
+
+$app->get("/register", function ($request, $response, $args) {
+    global $twig;
+    return $response->write($twig->render('register.html.twig', ['title' => 'Parkbud']));
+});
