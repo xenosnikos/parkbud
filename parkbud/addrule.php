15cb60e0544582cf06e288ffaca694333eecf4de
gataki <nxenos@me.com>
2021-05-18 10:20:55 -0400

    small push

--- parkbud/addrule.php
+++ parkbud/addrule.php
@@ -1,11 +1,44 @@
 <?php
 
+
+
+use Slim\Factory\AppFactory;
+use Slim\Views\TwigMiddleware;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
 require_once 'vendor/autoload.php';
 require_once 'init.php';
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 
-$app->get("/addrule", function ($request, $response, $args) {
+
+
+
+
+
+$app->get("/addrule", function ($request, $response, $args) use ($log){
+
+    $ruleLongitude = ;
+    $ruleLatitute = ;
+    $streetName = $request->getParam('streetname');
+    $startDate = $request->getParam('startDate');
+    $endDate = $request->getParam('endDate');
+    //$parkMeter = $request->getParam('endDate');
+    $ruleSide = ;
+    $ruleDay = ;
+    $ruleStartTime =;
+    $ruleEndTime =;
+
+    $errorList = [];
+
+
+
     global $twig;
     return $response->write($twig->render('addrule.html.twig', ['title' => 'Parkbud']));
 });
