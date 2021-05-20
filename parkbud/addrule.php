451baddc02f13c0d3062b73cb06b6e6007fe94ef
gataki <nxenos@me.com>
2021-05-19 21:46:13 -0400

    add rule DONE!

--- parkbud/addrule.php
+++ parkbud/addrule.php
@@ -3,6 +3,8 @@
 
 
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require_once 'vendor/autoload.php';
 require_once 'init.php';
@@ -12,26 +14,62 @@ require_once 'init.php';
 
 
 
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+$app->get("/addrule", function ($request, $response, $args) {
+    global $twig;
+    // return $response->write($twig->render('register.html.twig', ['title' => 'Parkbud']));
+    return $this->view->render($response, 'addrule.html.twig');
+});
 
-
-$app->get("/addrule", function ($request, $response, $args) use ($log){
-/*
-    $ruleLongitude = ;
-    $ruleLatitute = ;
-    $streetName = $request->getParam('streetname');
-    $startDate = $request->getParam('startDate');
-    $endDate = $request->getParam('endDate');
-    //$parkMeter = $request->getParam('endDate');
-    $ruleSide = ;
-    $ruleDay = ;
-    $ruleStartTime = $request->getParam('streetname');
-    $ruleEndTime =;
-
+$app->post("/addrule", function ($request, $response, $args) use ($log){
+    
+    $streetName = $request->getParam('streetName');
+    $periodStart = $request->getParam('periodStart');
+    $periodEnd = $request->getParam('periodEnd');
+    $parkingMeter = $request->getParam('parkingMeter');
+    $sideFlag = $request->getParam('sideFlag');
+    $mondayStart = $request->getParam('mondayStart');
+    $mondayEnd = $request->getParam('mondayEnd');
+    $tuesdayStart = $request->getParam('tuesdayStart');
+    $tuesdayEnd = $request->getParam('tuesdayEnd');
+    $wednesdayStart = $request->getParam('wednesdayStart');
+    $wednesdayEnd = $request->getParam('wednesdayEnd');
+    $thursdayStart = $request->getParam('thursdayStart');
+    $thursdayEnd = $request->getParam('thursdayEnd');
+    $fridayStart = $request->getParam('fridayStart');
+    $fridayEnd = $request->getParam('fridayEnd');
+    $saturdayStart = $request->getParam('saturdayStart');
+    $saturdayEnd = $request->getParam('saturdayEnd');
+    $sundayStart = $request->getParam('sundayStart');
+    $sundayEnd = $request->getParam('sundayEnd');
+    $longitude = $request->getParam('longitude');
+    $latitude = $request->getParam('latitude');
     $errorList = [];
-
-
-
-    */
+    
+    DB::insert('addrule', [
+        'streetName' => $streetName,
+        'periodStart' => $periodStart,
+        'periodEnd' => $periodEnd,
+        'parkingMeter' => $parkingMeter,
+        'sideFlag' => $sideFlag,
+        'mondayStart' => $mondayStart,
+        'mondayEnd' => $mondayEnd,
+        'tuesdayStart' => $tuesdayStart,
+        'tuesdayEnd' => $tuesdayEnd,
+        'wednesdayStart' => $wednesdayStart,
+        'wednesdayEnd' => $wednesdayEnd,
+        'thursdayStart' => $thursdayStart,
+        'thursdayEnd' => $thursdayEnd,
+        'fridayStart' => $fridayStart,
+        'fridayEnd' => $fridayEnd,
+        'saturdayStart' => $saturdayStart,
+        'saturdayEnd' => $saturdayEnd,
+        'sundayStart' => $sundayStart,
+        'sundayEnd' => $sundayEnd,
+        'longitude' => $longitude,
+        'latitude' => $latitude,
+    ]);
 
     global $twig;
     return $this->view->render($response, 'addrule.html.twig');
